ac4af3d31d6753a30f51d83db2b286a477399a5e
Fabio Scagliola <scagliola@gmail.com>
2025-11-07 09:58:46 +0100

    Frontmatter matters

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # My little brother listens to
 
-Add your ListenBrainz API URL to the `.env` file and run the following commands:
+If you are my little brother and want to track your own listens, then this is the project for you!
+
+Well, actually, whatever your relation to me is, you may use this simple Python script to dump the last releases you tracked on ListenBrainz into a frontmatter format like the one my little brother uses in his [media log](https://blog.scaglio.id/medialog/).
+
+Copy `.env.dist` to `.env`, add your ListenBrainz API URL, and run the following commands:
 
 ```
 python3 -m venv .venv
--- my_little_brother_listens_to.py
+++ my_little_brother_listens_to.py
@@ -1,6 +1,5 @@
-import os
-
 import dotenv
+import os
 import requests
 
 
@@ -11,13 +10,19 @@ def main():
     response = requests.get(url)
     response.raise_for_status()
     json = response.json()
-    item_list = []
+    item_list = {}
     for listen in response.json()["payload"]["listens"]:
-        item = listen["track_metadata"]["release_name"]
+        item = listen["track_metadata"]["additional_info"]["release_mbid"]
         if item not in item_list:
-            item_list.append(item)
+            item_list[item] = listen["track_metadata"]
     for item in item_list:
-        print(f"## {item}")
+        print(f"+++")
+        print(f"title = '{item_list[item]['release_name']} - {item_list[item]['artist_name']}'")
+        print(f"date = YYYY-MM-DD #today")
+        print(f"mediatype = 'record'")
+        print(f"website = 'https://musicbrainz.org/release/{item}'")
+        print(f"rating = ''")
+        print(f"+++")
 
 
 if __name__ == "__main__":
